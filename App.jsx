bfb1a755763fe00cc4f67906474bf65f08454508
xlaysplays <grinauraschulz@gmail.com>
2025-09-21 14:35:44 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>🎶 Musician Hub</h1>
+      <p>Welcome to your Discord-like app for musicians!</p>
+      <p>Try visiting /community or /jam (SPA routing works!)</p>
+    </div>
+  );
+}
